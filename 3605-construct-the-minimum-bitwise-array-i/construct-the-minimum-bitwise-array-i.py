721ad9372a99792d232f5a251c24a34f8af8ded1
Geeth-18 <rgvarshini18@gmail.com>
2026-01-20 21:22:35 +0530

    Time: 171 ms (12.50%) | Memory: 12.6 MB (25.00%) - LeetSync

---
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minBitwiseArray(self, nums):
+        ans = []
+        for p in nums:
+            found = -1
+            for x in range(p + 1):
+                if (x | (x + 1)) == p:
+                    found = x
+                    break
+            ans.append(found)
+        return ans
